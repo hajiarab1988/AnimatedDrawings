9034096ec2ee7ba30f94351e4d1b6734e2277524
hajiarab1988 <39706772+hajiarab1988@users.noreply.github.com>
2023-12-27 14:54:13 +0330

    Update animated_drawing.py

--- animated_drawings/model/animated_drawing.py
+++ animated_drawings/model/animated_drawing.py
@@ -362,11 +362,11 @@ class AnimatedDrawing(Transform, TimeManager):
         img_dim = max(rotated_body.size)  
         padded_body = Image.new("RGBA", (img_dim, img_dim), color=(0, 0, 0, 0))
         padded_body.paste(rotated_body, (0, 0))
-        if (self.counter < 4):
+        if (self.counter < 3):
             self.counter = self.counter +1
             
         else:
-            self.counter = 4
+            self.counter = 3
         print("counter = " , self.counter)
             
         return padded_body
